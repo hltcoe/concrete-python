64916df1d318567c26f11a99061341d91f94b880
Craig Harman <craig@craigharman.net>
2014-04-20 14:30:56 -0400

    Added optional positional argument for JSON output file.
    If no output filename is given, concrete2json.py will dump the JSON to
    stdout.

--- scripts/concrete2json.py
+++ scripts/concrete2json.py
@@ -16,9 +16,15 @@ def main():
 
     parser = argparse.ArgumentParser(description="Pretty Print a Concrete Communication file")
     parser.add_argument('communication_file')
+    parser.add_argument('json_file', nargs='?', default='STDOUT')
     args = parser.parse_args()
 
-    print communication_file_to_json(args.communication_file)
+    if args.json_file == 'STDOUT':
+        print communication_file_to_json(args.communication_file)
+    else:
+        f = codecs.open(args.json_file, "w", encoding="utf-8")
+        f.write(communication_file_to_json(args.communication_file))
+        f.close()
 
 
 if __name__ == "__main__":

9a7357b55908f25ec9b758123bd8e4659671cd1a
Craig Harman <craig@craigharman.net>
2014-07-01 16:36:46 -0400

    Updated validation to work with new TokenList class

--- tests/test_validate_communication.py
+++ tests/test_validate_communication.py
@@ -148,7 +148,7 @@ class TestTextspanOffsets(unittest.TestCase):
     def create_sentence_with_token(self, sentence_start, sentence_ending, token_start, token_ending):
         token_textspan = concrete.spans.ttypes.TextSpan(start=token_start, ending=token_ending)
         token = concrete.structure.ttypes.Token(textSpan=token_textspan)
-        tokenization = concrete.structure.ttypes.Tokenization(tokenList=[token])
+        tokenization = concrete.structure.ttypes.Tokenization(tokenList=concrete.TokenList(tokenList=[token]))
         sentence_textspan = concrete.spans.ttypes.TextSpan(start=sentence_start, ending=sentence_ending)
         sentence = concrete.structure.ttypes.Sentence(
             tokenizationList=[tokenization],
@@ -158,7 +158,7 @@ class TestTextspanOffsets(unittest.TestCase):
 
 
 def read_test_comm():
-    communication_filename = "tests/testdata/agiga_dog-bites-man.concrete"
+    communication_filename = "tests/testdata/serif_dog-bites-man.concrete"
     comm = concrete.Communication()
     comm_bytestring = open(communication_filename).read()
     TSerialization.deserialize(comm, comm_bytestring)

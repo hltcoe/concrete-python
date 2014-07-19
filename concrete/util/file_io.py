40ee804687ac189239cd3c2960ea275fd6da932b
max thomas <maxjthomas@gmail.com>
2014-07-19 14:33:44 -0400

    Modify files to better support arbitrary concrete usage.

--- concrete/util/file_io.py
+++ concrete/util/file_io.py
@@ -3,8 +3,20 @@
 
 from thrift import TSerialization
 
-from concrete import Communication
+from concrete import Communication, TokenLattice
 
+
+def read_thrift_from_file(thrift_obj, filename):
+    """
+    Pass in a new thrift object and a filename. Read in bytes from disk that
+    match that thrift object.
+
+    Return the deserialized thrift object.
+    """
+    thrift_bytes = open(filename).read()
+    TSerialization.deserialize(thrift_obj, thrift_bytes)
+    thrift_bytes.close()
+    return thrift_obj
 
 def read_communication_from_file(communication_filename):
     """
@@ -12,7 +24,12 @@ def read_communication_from_file(communication_filename):
     reads the Communication from the file and returns an instantiated
     Communication instance.
     """
-    comm = Communication()
-    comm_bytestring = open(communication_filename).read()
-    TSerialization.deserialize(comm, comm_bytestring)
-    return comm
+    return read_thrift_from_file(Communication(), communication_filename)
+
+def read_tokenlattice_from_file(tokenlattice_filename):
+    """
+    Takes the filename of a serialized Concrete TokenLattice file,
+    reads the TokenLattice from the file and returns an instantiated
+    TokenLattice instance.
+    """
+    return read_thrift_from_file(TokenLattice(), tokenlattice_filename)
--- concrete/util/json_fu.py
+++ concrete/util/json_fu.py
@@ -6,7 +6,7 @@ import json
 from thrift import TSerialization
 from thrift.protocol import TJSONProtocol
 
-from concrete.util import read_communication_from_file
+from concrete.util import read_communication_from_file, read_tokenlattice_from_file
 
 
 def communication_file_to_json(communication_filename):
@@ -16,14 +16,23 @@ def communication_file_to_json(communication_filename):
     Communication.
     """
     comm = read_communication_from_file(communication_filename)
-    return communication_to_json(comm)
+    return thrift_to_json(comm)
+
+def tokenlattice_file_to_json(toklat_filename):
+    """
+    Takes a Communication filename, deserializes Communication from
+    file, returns a JSON string with the information in that
+    Communication.
+    """
+    toklat = read_tokenlattice_from_file(toklat_filename)
+    return thrift_to_json(toklat)
 
 
-def communication_to_json(comm):
+def thrift_to_json(tobj):
     """
-    Takes a Communication instance, returns a JSON string with the
-    information in that Communication.
+    Takes a Thrift instance, returns a JSON string with the
+    information in that Thrift object.
     """
-    comm_json_string = TSerialization.serialize(comm, TJSONProtocol.TSimpleJSONProtocolFactory())
-    comm_json = json.loads(comm_json_string)
-    return json.dumps(comm_json, indent=2, separators=(',', ': '), ensure_ascii=False, sort_keys=True)
+    thrift_json_string = TSerialization.serialize(tobj, TJSONProtocol.TSimpleJSONProtocolFactory())
+    thrift_json = json.loads(thrift_json_string)
+    return json.dumps(thrift_json, indent=2, separators=(',', ': '), ensure_ascii=False, sort_keys=True)

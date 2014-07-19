1cfee2e8526f18a218e5ea54469dd080eb026e3a
max thomas <maxjthomas@gmail.com>
2014-07-19 14:36:30 -0400

    Modify concrete2json.py to work with arbitrary concrete files.

--- scripts/concrete2json.py
+++ scripts/concrete2json.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python
 """
-Pretty-prints a Concrete Communication file as JSON
+Pretty-prints a Concrete file as JSON
 """
 
 import argparse
 import codecs
 import sys
 
-from concrete.util import communication_file_to_json
+from concrete.util import communication_file_to_json, tokenlattice_file_to_json
 
+type_to_fn = {}
+type_to_fn['communication'] = communication_file_to_json
+type_to_fn['tokenlattice'] = tokenlattice_file_to_json
 
 def main():
     # Make stdout output UTF-8, preventing "'ascii' codec can't encode" errors
     sys.stdout = codecs.getwriter('utf8')(sys.stdout)
 
-    parser = argparse.ArgumentParser(description="Pretty Print a Concrete Communication file")
-    parser.add_argument('communication_file')
+    parser = argparse.ArgumentParser(description="Pretty Print a Concrete file")
+    parser.add_argument('concrete_file')
+    parser.add_argument('concrete_type', default='communication')
     parser.add_argument('json_file', nargs='?', default='STDOUT')
     args = parser.parse_args()
 
+    if args.concrete_type not in type_to_fn.keys():
+        print "This script does not support file type: {}".format(args.concrete_type)
+        exit(1)
+
+    conc_obj = type_to_fn[args.concrete_type](args.concrete_file)
+
     if args.json_file == 'STDOUT':
-        print communication_file_to_json(args.communication_file)
+        print conc_obj
     else:
         f = codecs.open(args.json_file, "w", encoding="utf-8")
-        f.write(communication_file_to_json(args.communication_file))
+        f.write(conc_obj)
         f.close()
 
 

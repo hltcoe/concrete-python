3638aa0a2202a6dbcdf189e55b0979a2c1b087ab
Craig Harman <craig@craigharman.net>
2014-08-15 12:17:09 -0400

    Fixed another import issue with concrete.util.generate_UUID()

--- concrete/util/concrete_uuid.py
+++ concrete/util/concrete_uuid.py
@@ -1,9 +1,10 @@
 """
 """
 
-# This file must not be named 'uuid.py', otherwise 'import uuid' will
-# try to import this file instead of the Python standard library
-# module
+# Force 'import uuid' to import the Python standard library module
+# named "uuid", and not the "concrete.uuid" module
+from __future__ import absolute_import
+
 import uuid as python_uuid
 
 import concrete
---
+++ tests/test_uuid.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+"""
+"""
+
+import unittest
+
+from concrete import Communication
+from concrete.util import generate_UUID
+from concrete.validate import validate_communication
+
+class TestUUID(unittest.TestCase):
+    def test_generate_uuid(self):
+        comm = Communication()
+        comm.uuid = generate_UUID()
+
+    def test_minimial_communication_with_uuid(self):
+        comm = Communication()
+        comm.id = "myID"
+        comm.type = "Test Communication"
+        comm.uuid = generate_UUID()
+        self.assertTrue(validate_communication(comm))

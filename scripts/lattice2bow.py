789a7253116b6fb25b47778032bf4624ec018143
Chandler May <cjmay4754@gmail.com>
2014-09-18 22:26:39 -0400

    implement viterbi in lattice2bow and throw away other stuff (prep for merge lattice2bow_dyn)

--- scripts/lattice2bow.py
+++ scripts/lattice2bow.py
@@ -26,50 +26,32 @@ def group_arcs_by_src(lattice):
     return arcs_by_src
 
 
-def extract_paths(arcs_by_src):
-    paths = [[] for src in arcs_by_src]
-    for i in xrange(len(arcs_by_src) - 1, -1, -1): # TODO edge cases?
-        for arc in arcs_by_src[i]:
-            if arc.dst in arcs_by_src:
-                for (tail, weight) in paths[arc.dst]:
-                    paths[arc.src].append(([arc] + tail, arc.weight + weight))
-            else:
-                paths[arc.src].append(([arc], arc.weight))
+def find_best_path(arcs_by_src):
+    subpath_weight_pairs = {0: ([], 0.)} # indexed by dst
+    farthest_reachable_dst = 0
 
-    if paths:
-        return paths[0]
-    else:
-        return []
+    for i in xrange(len(arcs_by_src)):
+        if i in subpath_weight_pairs: # reachable
+            (subpath, weight) = subpath_weight_pairs[i]
+            for arc in arcs_by_src[i]:
+                new_subpath = subpath + [arc]
+                new_weight = arc.weight + weight
+                new_pair = (new_subpath, new_weight)
+                if arc.dst in subpath_weight_pairs:
+                    if new_weight > subpath_weight_pairs[arc.dst][1]:
+                        subpath_weight_pairs[arc.dst] = new_pair
+                        farthest_reachable_dst = max(farthest_reachable_dst,
+                                                     arc.dst)
+                else:
+                    subpath_weight_pairs[arc.dst] = new_pair
+                    farthest_reachable_dst = max(farthest_reachable_dst,
+                                                 arc.dst)
 
-
-def compute_expected_counts(path_weight_pairs):
-    normalizer = math.log(sum(math.exp(w) for (p, w) in path_weight_pairs))
-    counts = dict()
-    for (path, weight) in path_weight_pairs:
-        p = math.exp(weight - normalizer)
-        for arc in path:
-            token = arc.token
-            if token.text in counts:
-                counts[token.text] += p
-            else:
-                counts[token.text] = p
-    return counts
+    return subpath_weight_pairs[farthest_reachable_dst]
 
 
-def compute_counts(path):
-    counts = dict()
-    for arc in path:
-        token = arc.token
-        if token.text in counts:
-            counts[token.text] += 1
-        else:
-            counts[token.text] = 1
-    return counts
-
-
-def write_best_path(output_path, lattice, path):
-    lattice.cachedBestPath = LatticePath(tokenList=[Token(text=arc.token.text)
-                                                    for arc in path])
+def cache_best_path(output_path, lattice, path):
+    lattice.cachedBestPath = LatticePath(tokenList=[arc.token for arc in path])
     with open(output_path, 'wb') as f:
         transport = TTransport.TFileObjectTransport(f)
         protocol = TBinaryProtocol.TBinaryProtocol(transport)
@@ -79,39 +61,23 @@ def write_best_path(output_path, lattice, path):
 def main():
     from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
     parser = ArgumentParser(formatter_class=ArgumentDefaultsHelpFormatter)
-    parser.set_defaults(raw=False, best=False, write=False)
+    parser.set_defaults(raw=False, best=False, cache_best=False)
     parser.add_argument('input_path', type=str,
                         help='path to serialized concrete TokenLattice')
-    parser.add_argument('--raw', action='store_true',
-                        help='return all paths')
     parser.add_argument('--best', action='store_true',
-                        help='return counts for best path')
-    parser.add_argument('--write', action='store_true',
-                        help='write back to lattice')
+                        help='print best path')
+    parser.add_argument('--cache_best', action='store_true',
+                        help='cache best path to lattice')
     args = parser.parse_args()
 
     lattice = load_lattice(args.input_path)
     arcs_by_src = group_arcs_by_src(lattice)
-    path_weight_pairs = extract_paths(arcs_by_src)
-    if args.raw:
-        path_weight_pairs.sort(key=lambda pair: pair[1], reverse=True)
-        for (path, weight) in path_weight_pairs:
-            print (path, weight)
-    else:
-        if args.best:
-            path = reduce(lambda p, q: (p[1] > q[1]) and p or q,
-                          path_weight_pairs)
-            if args.write:
-                write_best_path(args.input_path, lattice, path[0])
-            counts = compute_counts(path[0])
+    if args.best or args.cache_best:
+        (path, weight) = find_best_path(arcs_by_src)
+        if args.cache_best:
+            cache_best_path(args.input_path, lattice, path)
         else:
-            counts = compute_expected_counts(path_weight_pairs)
-
-        if not args.write:
-            counts_items = counts.items()
-            counts_items.sort(key=lambda pair: pair[1], reverse=True)
-            for (w, p) in counts_items:
-                print '%.9f %s' % (p, w)
+            print ' '.join(arc.token.text for arc in path)
 
 
 if __name__ == '__main__':

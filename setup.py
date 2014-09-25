fb12467217fee521080f13010cf3cbf81366a6d3
Craig Harman <craig@craigharman.net>
2014-09-25 12:22:37 -0400

    Bumped version number to v4.0.0pre

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name = "concrete",
-    version = "3.4.0pre",
+    version = "4.0.0pre",
     description = "Python modules and scripts for working with Concrete",
 
     packages = [

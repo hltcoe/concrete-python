2953ca49defc4ced96fd458e90fa9775b68cceaa
Craig Harman <craig@craigharman.net>
2014-11-07 16:00:57 -0500

    Modified read_thrift_from_file() to use TBinaryProtocol if TCompactProtocol does not work

--- concrete/util/file_io.py
+++ concrete/util/file_io.py
@@ -18,6 +18,14 @@ def read_thrift_from_file(thrift_obj, filename):
     thrift_file = open(filename)
     thrift_bytes = thrift_file.read()
     TSerialization.deserialize(thrift_obj, thrift_bytes, protocol_factory=TCompactProtocol.TCompactProtocolFactory())
+    # TODO: Implement something more elegant than this kludge
+    # If we try to use TCompactProtocol to read a TBinaryProtocol Thrift object,
+    # TSerializationdeserialize() will not give any indication that something
+    # has gone wrong.  We currently check if the required field 'id' is empty,
+    # and if so, we then try to read the Thrift object using the (default)
+    # TBinaryProtocol protocol.
+    if thrift_obj.id == None:
+        TSerialization.deserialize(thrift_obj, thrift_bytes)
     thrift_file.close()
     return thrift_obj
 

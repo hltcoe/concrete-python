2728729a59659c1170795a6ef3a9185f0a1a50d4
Craig Harman <craig@craigharman.net>
2015-02-04 14:59:50 -0500

    Added script for creating Communication archive test files
    The data created by the script 'create_simple_communications.py'
    is used to test the CommunicationReader class.

---
+++ tests/testdata/create_simple_communications.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""Script to create 'simple*' test files containing multiple Communications
+
+These test files are used by the test script:
+
+  tests/test_file_io.py
+
+This script should *only* be re-run if there are major breaking
+changes to the Concrete specification that cause the existing sample
+Communications files to be rejected by the Concrete validator.  This
+script is not intended to be run as part of the standard test suite.
+
+This Python script is secretly a shell script.
+"""
+
+import os
+
+from concrete.util import write_communication_to_file
+from concrete.util.simple_comm import create_simple_comm
+
+n1 = 'simple_1.concrete'
+n2 = 'simple_2.concrete'
+n3 = 'simple_3.concrete'
+
+write_communication_to_file(create_simple_comm('one'), n1)
+write_communication_to_file(create_simple_comm('two'), n2)
+write_communication_to_file(create_simple_comm('three'), n3)
+
+os.system('gzip --keep %s' % n1)
+os.system('cat %s %s %s > simple_concatenated' % (n1, n2, n3))
+os.system('gzip --keep simple_concatenated')
+os.system('tar cvf simple.tar %s %s %s' % (n1, n2, n3))
+os.system('gzip --keep simple.tar')
+os.system('zip simple.zip %s %s %s' % (n1, n2, n3))

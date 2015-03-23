243959a7cf6477e497252c09e0cf26d78d42c16d
Craig Harman <craig@craigharman.net>
2015-03-23 11:37:29 -0400

    JSON to Concrete Tweet conversion code now imports TwitterEntities

--- concrete/util/twitter.py
+++ concrete/util/twitter.py
@@ -56,12 +56,34 @@ def json_tweet_object_to_TweetInfo(tweet):
                     logging.warn("Concrete schema for '%s' missing field for Twitter API field '%s'" %
                                  (type(concrete_object), key))
 
-    twitter_user = TwitterUser()
-    set_flat_fields(twitter_user, tweet[u'user'])
-
     tweet_info = TweetInfo()
     set_flat_fields(tweet_info, tweet)
+
+    twitter_user = TwitterUser()
+    set_flat_fields(twitter_user, tweet[u'user'])
     tweet_info.user = twitter_user
+
+    if tweet[u'entities']:
+        twitter_entities = TwitterEntities()
+        if tweet[u'entities'][u'hashtags']:
+            twitter_entities.hashtagList = []
+            for hashtag_dict in tweet[u'entities'][u'hashtags']:
+                hashtag = HashTag()
+                set_flat_fields(hashtag, hashtag_dict)
+                twitter_entities.hashtagList.append(hashtag)
+        if tweet[u'entities'][u'urls']:
+            twitter_entities.urlList = []
+            for url_dict in tweet[u'entities'][u'urls']:
+                url = URL()
+                set_flat_fields(url, url_dict)
+                twitter_entities.urlList.append(url)
+        if tweet[u'entities'][u'user_mentions']:
+            twitter_entities.userMentionList = []
+            for user_mention_dict in tweet[u'entities'][u'user_mentions']:
+                user_mention = UserMention()
+                set_flat_fields(user_mention, user_mention_dict)
+                twitter_entities.userMentionList.append(user_mention)
+        tweet_info.entities = twitter_entities
 
     return tweet_info
 

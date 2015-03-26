1bbe16cc2f73918a7e7726234e653eaf83de1e4a
Craig Harman <craig@craigharman.net>
2015-03-26 12:34:11 -0400

    Added '--remove-timestamps' flag to concrete2json.py script

--- scripts/concrete2json.py
+++ scripts/concrete2json.py
@@ -25,6 +25,7 @@ def main():
                         help='Default: communication')
     parser.add_argument('--protocol', default='simple', choices=['simple', 'TJSONProtocol'],
                         help='Default: simple')
+    parser.add_argument('--remove-timestamps', action='store_true', help="Removes timestamps from JSON output")
     parser.add_argument('--remove-uuids', action='store_true', help="Removes UUIDs from JSON output")
     parser.add_argument('concrete_file')
     parser.add_argument('json_file', nargs='?', default='STDOUT')
@@ -32,7 +33,9 @@ def main():
 
     if args.protocol == 'simple':
         if args.concrete_type == 'communication':
-            json_communication = communication_file_to_json(args.concrete_file, args.remove_uuids)
+            json_communication = communication_file_to_json(args.concrete_file,
+                                                            remove_timestamps=args.remove_timestamps,
+                                                            remove_uuids=args.remove_uuids)
         else:
             json_communication = tokenlattice_file_to_json(args.concrete_file)
     else:

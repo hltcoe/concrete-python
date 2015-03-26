86d15ddfb4b2203170a01f1f4f5b92cb6842bc55
Craig Harman <craig@craigharman.net>
2015-03-26 11:18:55 -0400

    Added 'remove_uuids' flag to communication_file_to_json(), thrift_to_json()

--- concrete/util/json_fu.py
+++ concrete/util/json_fu.py
@@ -9,19 +9,20 @@ from thrift.protocol import TJSONProtocol
 from concrete.util import read_communication_from_file, read_tokenlattice_from_file
 
 
-def communication_file_to_json(communication_filename):
+def communication_file_to_json(communication_filename, remove_uuids=False):
     """Get a "pretty-printed" JSON string representation for a Communication
 
     Args:
 
     - `communication_filename`: String specifying Communication filename
+    - `remove_uuids`: Boolean flag indicating if Concrete UUIDs should be removed
 
     Returns:
 
     - A string containing a "pretty-printed" JSON representation of the Communication
     """
     comm = read_communication_from_file(communication_filename)
-    return thrift_to_json(comm)
+    return thrift_to_json(comm, remove_uuids)
 
 def tokenlattice_file_to_json(toklat_filename):
     """Get a "pretty-printed" JSON string representation for a TokenLattice
@@ -38,12 +39,39 @@ def tokenlattice_file_to_json(toklat_filename):
     return thrift_to_json(toklat)
 
 
-def thrift_to_json(tobj):
+def get_json_object_without_uuids(json_object):
+    """Create a copy of a JSON object created by json.loads(), with all
+    representations of Concrete UUIDs (dictionaries containing a
+    'uuidString' key) recursively removed.
+
+    Args:
+
+    - `json_object`: A Python data structure created from a JSON string by json.loads
+
+    Returns:
+
+    - A copy of the input data structure with all UUID objects removed
+    """
+    if type(json_object) is list:
+        new_json_object = [get_json_object_without_uuids(obj) for obj in json_object]
+    elif type(json_object) is dict:
+        new_json_object = {}
+        for k, v in json_object.iteritems():
+            if type(v) is not dict or 'uuidString' not in v:
+                new_json_object[k] = get_json_object_without_uuids(v)
+    else:
+        new_json_object = json_object
+
+    return new_json_object
+
+
+def thrift_to_json(tobj, remove_uuids=False):
     """Get a "pretty-printed" JSON string representation for a Thrift object
 
     Args:
 
     - `tobj`: A Thrift object
+    - `remove_uuids`: Boolean flag indicating if Concrete UUIDs should be removed
 
     Returns:
 
@@ -51,4 +79,8 @@ def thrift_to_json(tobj):
     """
     thrift_json_string = TSerialization.serialize(tobj, TJSONProtocol.TSimpleJSONProtocolFactory())
     thrift_json = json.loads(thrift_json_string)
+
+    if remove_uuids:
+        thrift_json = get_json_object_without_uuids(thrift_json)
+
     return json.dumps(thrift_json, indent=2, separators=(',', ': '), ensure_ascii=False, sort_keys=True)

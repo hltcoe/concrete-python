d9ca2de9dcf145729d29e9078f374f735cd0e739
Chandler May <cjmay4754@gmail.com>
2015-09-26 09:02:14 -0400

    little changes to classes: remove obsolete discourse, update imports in init

--- concrete/__init__.py
+++ concrete/__init__.py
@@ -10,14 +10,14 @@ specification defined using [Thrift](http://thrift.apache.org).
 from audio.ttypes import *
 from clustering.ttypes import *
 from communication.ttypes import *
-from communication_service.ttypes import *
-from discourse.ttypes import *
 from email.ttypes import *
 from entities.ttypes import *
+from exceptions.ttypes import *
 from language.ttypes import *
 from linking.ttypes import *
 from metadata.ttypes import *
 from nitf.ttypes import *
+from services.ttypes import *
 from situations.ttypes import *
 from spans.ttypes import *
 from structure.ttypes import *

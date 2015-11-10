712c16fe2214f185bbc35e3c972aca00204dfe87
Joel Coffman <joel.coffman@jhuapl.edu>
2015-11-10 13:48:37 -0500

    Suppress PEP 8 import warnings in concrete.util
    This change suppresses the PEP 8 import warnings due to wildcard
    import statements in concrete/util/__init__.py.

--- concrete/util/__init__.py
+++ concrete/util/__init__.py
@@ -1,10 +1,10 @@
 """Utility code for working with Concrete
 """
 
-from concrete_uuid import *
-from file_io import *
-from redis_io import *
-from json_fu import *
-from references import *
-from tokenization import *
-from unnone import *
+from concrete_uuid import *  # noqa
+from file_io import *  # noqa
+from redis_io import *  # noqa
+from json_fu import *  # noqa
+from references import *  # noqa
+from tokenization import *  # noqa
+from unnone import *  # noqa

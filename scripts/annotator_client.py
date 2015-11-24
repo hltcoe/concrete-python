2d7c0b30e7dfce20df2566d4d6304267225065d8
Chandler May <cjmay4754@gmail.com>
2015-11-24 16:16:47 +0000

    Simplify annotator_client using CommunicationReader, CommunicationWriter.

--- scripts/annotator_client.py
+++ scripts/annotator_client.py
@@ -1,57 +1,32 @@
 #!/usr/bin/env python
 
-import argparse
-import codecs
-import os
-import sys
-
-from thrift import TSerialization
-from thrift.protocol import TCompactProtocol
-
-import concrete
-from concrete.util import CommunicationWriter
-from concrete.util import read_communication_from_file
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from concrete.util import CommunicationReader, CommunicationWriter
 from concrete.util.annotator_wrapper import AnnotatorClientWrapper
 
 
 def main():
-    # Make stdout output UTF-8, preventing "'ascii' codec can't encode" errors
-    sys.stdout = codecs.getwriter('utf8')(sys.stdout)
-
-    parser = argparse.ArgumentParser(description="Interface with a Concrete Annotator service")
-    parser.add_argument('--port', required=True, type=int, help="Port to use.")
-    parser.add_argument('--host', required=True, default='localhost', help="Hostname to use.")
-    parser.add_argument('--input', default='-', help="Input source to use. '-' for stdin; otherwise takes a path to a file. [Default: stdin]")
-    parser.add_argument('--output', default='-', help="Output source to use. '-' for stdout; otherwise takes a path to a file. [Default: stdout]")
+    parser = ArgumentParser(
+        formatter_class=ArgumentDefaultsHelpFormatter,
+        description="Interface with a Concrete Annotator service"
+    )
+    parser.add_argument('host', help="Hostname of annotator service to which to connect.")
+    parser.add_argument('port', type=int, help="Port of annotator service to which to connect.")
+    parser.add_argument('--input', default='-', help="Input source to use. '-' for stdin; otherwise takes a path to a file.")
+    parser.add_argument('--output', default='-', help="Output source to use. '-' for stdout; otherwise takes a path to a file.")
     args = parser.parse_args()
 
-    if args.input == '-':
-        # ???
-        handle = os.fdopen(sys.stdin.fileno(), 'rb')
-        try:
-            bytez = handle.read()
-            comm = concrete.Communication()
-            TSerialization.deserialize(comm, bytez, protocol_factory=TCompactProtocol.TCompactProtocolFactory())
-        finally:
-            handle.close()
-    else:
-        comm = read_communication_from_file(args.input)
+    # Won't work on Windows...
+    # but stdin/stdout pipelines using this script in Windows have probability 0
+    input_path = '/dev/fd/0' if (args.input) == '-' else args.input
+    output_path = '/dev/fd/1' if (args.output) == '-' else args.output
 
+    reader = CommunicationReader(input_path)
     with AnnotatorClientWrapper(args.host, args.port) as client:
-        new_comm = client.annotate(comm)
+        with CommunicationWriter(output_path) as writer:
+            for (comm, _) in reader:
+                writer.write(client.annotate(comm))
 
-    if args.output == '-':
-        new_bytes = TSerialization.serialize(new_comm, protocol_factory=TCompactProtocol.TCompactProtocolFactory())
-        handle = os.fdopen(sys.stdout.fileno(), 'wb')
-        try:
-            handle.write(new_bytes)
-        finally:
-            handle.close()
-
-    else:
-        wrtr = CommunicationWriter(args.output)
-        wrtr.write(new_comm)
-        wrtr.close()
 
 if __name__ == "__main__":
     main()

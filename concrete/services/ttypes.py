1b2355ce7882780451653ed815beecb648da131a
Chandler May <cjmay4754@gmail.com>
2016-12-20 01:30:26 -0500

    build for concrete 71ad827 services aa908de (4.11)

--- concrete/services/ttypes.py
+++ concrete/services/ttypes.py
@@ -139,6 +139,92 @@ class ServicesException(TException):
   def __ne__(self, other):
     return not (self == other)
 
+class NotImplementedException(TException):
+  """
+  An exception to be used when an invoked method has
+  not been implemented by the service.
+
+  Attributes:
+   - message: The explanation (why the exception occurred)
+   - serEx: The serialized exception
+  """
+
+  thrift_spec = (
+    None, # 0
+    (1, TType.STRING, 'message', None, None, ), # 1
+    (2, TType.STRING, 'serEx', None, None, ), # 2
+  )
+
+  def __init__(self, message=None, serEx=None,):
+    self.message = message
+    self.serEx = serEx
+
+  def read(self, iprot):
+    if iprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None and fastbinary is not None:
+      fastbinary.decode_binary(self, iprot.trans, (self.__class__, self.thrift_spec))
+      return
+    iprot.readStructBegin()
+    while True:
+      (fname, ftype, fid) = iprot.readFieldBegin()
+      if ftype == TType.STOP:
+        break
+      if fid == 1:
+        if ftype == TType.STRING:
+          self.message = iprot.readString().decode('utf-8')
+        else:
+          iprot.skip(ftype)
+      elif fid == 2:
+        if ftype == TType.STRING:
+          self.serEx = iprot.readString()
+        else:
+          iprot.skip(ftype)
+      else:
+        iprot.skip(ftype)
+      iprot.readFieldEnd()
+    iprot.readStructEnd()
+
+  def write(self, oprot):
+    if oprot.__class__ == TBinaryProtocol.TBinaryProtocolAccelerated and self.thrift_spec is not None and fastbinary is not None:
+      oprot.trans.write(fastbinary.encode_binary(self, (self.__class__, self.thrift_spec)))
+      return
+    oprot.writeStructBegin('NotImplementedException')
+    if self.message is not None:
+      oprot.writeFieldBegin('message', TType.STRING, 1)
+      oprot.writeString(self.message.encode('utf-8'))
+      oprot.writeFieldEnd()
+    if self.serEx is not None:
+      oprot.writeFieldBegin('serEx', TType.STRING, 2)
+      oprot.writeString(self.serEx)
+      oprot.writeFieldEnd()
+    oprot.writeFieldStop()
+    oprot.writeStructEnd()
+
+  def validate(self):
+    if self.message is None:
+      raise TProtocol.TProtocolException(message='Required field message is unset!')
+    return
+
+
+  def __str__(self):
+    return repr(self)
+
+  def __hash__(self):
+    value = 17
+    value = (value * 31) ^ hash(self.message)
+    value = (value * 31) ^ hash(self.serEx)
+    return value
+
+  def __repr__(self):
+    L = ['%s=%r' % (key, value)
+      for key, value in self.__dict__.iteritems()]
+    return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+
+  def __eq__(self, other):
+    return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+  def __ne__(self, other):
+    return not (self == other)
+
 class AsyncContactInfo(object):
   """
   Contact information for the asynchronous communications.

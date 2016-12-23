a4afac4b1e5b78bec456a7bbf77ae1d267c5508c
Chandler May <cjmay4754@gmail.com>
2016-12-23 10:06:14 -0500

    move lattice2bow logic to tokenization, remove script

--- concrete/util/tokenization.py
+++ concrete/util/tokenization.py
@@ -98,3 +98,138 @@ def get_comm_tokenizations(comm, tool=None):
         for sentence in lun(section.sentenceList):
             if tool is None or sentence.tokenization.metadata.tool == tool:
                 yield sentence.tokenization
+
+
+def latticeToFsm(lattice, is_cost=True):
+    '''
+    Author: Adrian Benton
+    '''
+    fsm = {}
+    bkFsm = {}
+
+    tokens = set()
+    states = set()
+    for arc in lattice.arcList:
+        src, dst, token, wt = arc.src, arc.dst, arc.token.text, arc.weight
+        tokens.add(token)
+        states.add(src)
+        states.add(dst)
+
+        if is_cost:  # Treat as log-probs instead
+            wt = -wt
+
+        if src not in fsm:
+            fsm[src] = {}
+        if dst not in fsm[src]:
+            fsm[src][dst] = []
+        fsm[src][dst].append((token, wt))
+
+        if dst not in bkFsm:
+            bkFsm[dst] = {}
+        if src not in bkFsm[dst]:
+            bkFsm[dst][src] = []
+        bkFsm[dst][src].append((token, wt))
+
+    return fsm, bkFsm, tokens, states
+
+
+def calc_alpha(fsm, states, start, end):
+    '''
+    Calculates total in-probability of each state.  Assumes weights are
+    log-probbish
+    Author: Adrian Benton
+    '''
+
+    from scipy.misc import logsumexp
+
+    alpha = {}
+    alpha[start] = 0.
+
+    for state in states:
+        currState = state
+
+        if currState == end:
+            continue
+
+        arcs = fsm[currState]
+
+        for dst, tokenWts in arcs.items():
+            if dst not in alpha:
+                alpha[dst] = float('-inf')
+            for token, wt in tokenWts:
+                alpha[dst] = logsumexp([alpha[dst], alpha[currState] + wt])
+
+    return alpha
+
+
+def calc_beta(bkFsm, states, end, start):
+    '''
+    Calculates total out-probability of each state.  Assumes weights are
+    log-probbish
+    Author: Adrian Benton
+    '''
+
+    from scipy.misc import logsumexp
+
+    beta = {}
+    beta[end] = 0.
+
+    for state in sorted(states, reverse=True):
+        currState = state
+
+        if currState == start:
+            continue
+
+        arcs = bkFsm[currState]
+
+        for src, tokenWts in arcs.items():
+            if src not in beta:
+                beta[src] = float('-inf')
+            for token, wt in tokenWts:
+                beta[src] = logsumexp([beta[src], beta[currState] + wt])
+
+    return beta
+
+
+def get_norm(alpha, beta, start, end):
+    '''
+    Author: Adrian Benton
+    '''
+    return alpha[end]
+
+
+def compute_lattice_expected_counts(lattice, is_cost=True):
+    '''
+    Assumes that edge weights are in log-space (may be + or -).
+    Author: Adrian Benton
+    '''
+
+    from scipy.misc import logsumexp
+
+    fsm, bkFsm, tokens, states = latticeToFsm(lattice, is_cost=is_cost)
+    states = sorted(list(states))
+    alpha = calc_alpha(fsm, states, lattice.startState, lattice.endState)
+    beta = calc_beta(bkFsm, states, lattice.endState, lattice.startState)
+    norm = get_norm(alpha, beta, lattice.startState, lattice.endState)
+
+    expectedCounts = {}
+
+    for state in states:
+        currState = state
+
+        if currState == lattice.endState:
+            continue
+
+        arcs = fsm[currState]
+
+        for dst, tokenWts in arcs.items():
+            for token, wt in tokenWts:
+                if token not in expectedCounts:
+                    expectedCounts[token] = []
+                expectedCounts[token].append(
+                    alpha[currState] + beta[dst] + wt - norm)
+
+    for token in expectedCounts:
+        expectedCounts[token] = logsumexp(expectedCounts[token])
+
+    return expectedCounts

a26e836fdbc6452185e94f3ec4b78563246d766e
Craig Harman <craig@craigharman.net>
2017-01-23 07:49:34 -0500

    Refactored SearchService/AnnotateCommunicationService wrappers

--- concrete/util/annotate_wrapper.py
+++ concrete/util/annotate_wrapper.py
@@ -1,93 +1,23 @@
-from multiprocessing import Process
-from time import sleep
-from socket import create_connection
-
 from concrete.annotate import AnnotateCommunicationService
-from concrete.util.thrift_factory import factory
+from concrete.util.service_wrapper import (
+    ConcreteServiceClientWrapper,
+    ConcreteServiceWrapper,
+    SubprocessConcreteServiceWrapper)
 
 
-class AnnotateCommunicationClientWrapper(object):
-    """
-    A sample client implementation of the Concrete
-    AnnotateCommunicationService.
-
-    Provides sensible/current defaults for transport and protocol.
-    """
-
-    def __init__(self, host, port):
-        self.host = host
-        self.port = port
-
-    def __enter__(self):
-        socket = factory.createSocket(self.host, self.port)
-        self.transport = factory.createTransport(socket)
-        protocol = factory.createProtocol(self.transport)
-
-        cli = AnnotateCommunicationService.Client(protocol)
-
-        self.transport.open()
-        return cli
-
-    def __exit__(self, type, value, traceback):
-        self.transport.close()
+AnnotateCommunicationClientWrapper = type(
+    'AnnotateCommunicationClientWrapper',
+    (ConcreteServiceClientWrapper,),
+    {'concrete_service_class': AnnotateCommunicationService})
 
 
-class AnnotateCommunicationServiceWrapper(object):
-    """
-    A sample wrapper around the Concrete annotator
-    service, providing an easy way to wrap an implementation
-    of the AnnotateCommunicationService.
-    """
-
-    def __init__(self, implementation):
-        self.processor = AnnotateCommunicationService.Processor(implementation)
-
-    def serve(self, host, port):
-        server = factory.createServer(self.processor, host, port)
-
-        # NOTE: Thrift's servers run indefinitely. This server implementation
-        # may be killed by a KeyboardInterrupt (Control-C); otherwise, the
-        # process must be killed to terminate the server.
-        server.serve()
+AnnotateCommunicationServiceWrapper = type(
+    'AnnotateCommunicationServiceWrapper',
+    (ConcreteServiceWrapper,),
+    {'concrete_service_class': AnnotateCommunicationService})
 
 
-class SubprocessAnnotateCommunicationServiceWrapper(object):
-    '''
-    AnnotateCommunicationService service wrapper that runs server in a
-    subprocess via a context manager interface.
-    '''
-
-    SLEEP_INTERVAL = 0.1
-
-    def __init__(self, implementation, host, port, timeout=None):
-        self.proc = None
-        self.server = AnnotateCommunicationServiceWrapper(implementation)
-        self.host = host
-        self.port = port
-        self.timeout = timeout
-
-    def __enter__(self):
-        self.proc = Process(target=self.server.serve,
-                            args=(self.host, self.port))
-        self.proc.start()
-
-        s = None
-        elapsed = 0.
-
-        # Loop until successful connect or timeout
-        while s is None and (self.timeout is None or elapsed < self.timeout):
-            try:
-                s = create_connection((self.host, self.port))
-                s.close()
-            except:
-                s = None
-
-                # this is not precise... not important
-                sleep(self.SLEEP_INTERVAL)
-                elapsed += self.SLEEP_INTERVAL
-
-        return self
-
-    def __exit__(self, type, value, traceback):
-        self.proc.terminate()
-        self.proc.join()
+SubprocessAnnotateCommunicationServiceWrapper = type(
+    'SubprocessAnnotateCommunicationServiceWrapper',
+    (SubprocessConcreteServiceWrapper,),
+    {'concrete_service_wrapper_class': AnnotateCommunicationServiceWrapper})

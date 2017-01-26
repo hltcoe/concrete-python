01405287821d135b882ce8224f9b13bcdf0cf35f
Chandler May <cjmay4754@gmail.com>
2017-01-26 17:21:57 -0500

    use pytest in test_comm_container

--- tests/test_comm_container.py
+++ tests/test_comm_container.py
@@ -1,5 +1,3 @@
-import unittest
-
 from concrete.util.comm_container import (
     DirectoryBackedCommunicationContainer,
     MemoryBackedCommunicationContainer,
@@ -8,45 +6,46 @@ from concrete.util.comm_container import (
 
 from concrete.validate import validate_communication
 
-
-class TestDirectoryBackedCommunicationContainer(unittest.TestCase):
-    def test_find_files_recursively(self):
-        directory_path = u'tests/testdata/a'
-        cc = DirectoryBackedCommunicationContainer(directory_path)
-        self.assertEqual(3, len(cc))
-
-    def test_retrieve(self):
-        directory_path = u'tests/testdata/a'
-        cc = DirectoryBackedCommunicationContainer(directory_path)
-        self.assertEqual(3, len(cc))
-        self.assertTrue(u'simple_1' in cc)
-        for comm_id in cc:
-            comm = cc[comm_id]
-            self.assertTrue(validate_communication(comm))
+from pytest import raises
 
 
-class TestMemoryBackedCommunicationContainer(unittest.TestCase):
-    def test_filesize_check(self):
-        comm_path = u'tests/testdata/simple.tar.gz'
-        with self.assertRaises(Exception):
-            MemoryBackedCommunicationContainer(comm_path, max_file_size=500)
-
-    def test_retrieve(self):
-        comm_path = u'tests/testdata/simple.tar.gz'
-        cc = MemoryBackedCommunicationContainer(comm_path)
-        self.assertEqual(3, len(cc))
-        self.assertTrue(u'one' in cc)
-        for comm_id in cc:
-            comm = cc[comm_id]
-            self.assertTrue(validate_communication(comm))
+def test_directory_backed_comm_container_find_files_recursively():
+    directory_path = u'tests/testdata/a'
+    cc = DirectoryBackedCommunicationContainer(directory_path)
+    assert 3 == len(cc)
 
 
-class TestZipFileBackedCommunicationContainer(unittest.TestCase):
-    def test_retrieve(self):
-        zipfile_path = u'tests/testdata/simple.zip'
-        cc = ZipFileBackedCommunicationContainer(zipfile_path)
-        self.assertEqual(3, len(cc))
-        self.assertTrue(u'simple_1' in cc)
-        for comm_id in cc:
-            comm = cc[comm_id]
-            self.assertTrue(validate_communication(comm))
+def test_directory_backed_comm_container_retrieve():
+    directory_path = u'tests/testdata/a'
+    cc = DirectoryBackedCommunicationContainer(directory_path)
+    assert 3 == len(cc)
+    assert u'simple_1' in cc
+    for comm_id in cc:
+        comm = cc[comm_id]
+        assert validate_communication(comm)
+
+
+def test_memory_backed_comm_container_filesize_check():
+    comm_path = u'tests/testdata/simple.tar.gz'
+    with raises(Exception):
+        MemoryBackedCommunicationContainer(comm_path, max_file_size=500)
+
+
+def test_memory_backed_comm_container_retrieve():
+    comm_path = u'tests/testdata/simple.tar.gz'
+    cc = MemoryBackedCommunicationContainer(comm_path)
+    assert 3 == len(cc)
+    assert u'one' in cc
+    for comm_id in cc:
+        comm = cc[comm_id]
+        assert validate_communication(comm)
+
+
+def test_zip_file_backed_comm_container_retrieve():
+    zipfile_path = u'tests/testdata/simple.zip'
+    cc = ZipFileBackedCommunicationContainer(zipfile_path)
+    assert 3 == len(cc)
+    assert u'simple_1' in cc
+    for comm_id in cc:
+        comm = cc[comm_id]
+        assert validate_communication(comm)

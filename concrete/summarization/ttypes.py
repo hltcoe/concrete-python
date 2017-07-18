ce24f29bb6043a26cae329d72429ad41ee7073e4
Craig Harman <craig@craigharman.net>
2017-07-17 20:34:18 -0400

    Regenerated concrete/summarization/ttypes.py from tweaked 4.13 schema

--- concrete/summarization/ttypes.py
+++ concrete/summarization/ttypes.py
@@ -183,93 +183,6 @@ class SummarizationRequest(object):
         return not (self == other)
 
 
-class Summary(object):
-    """
-    A shortened version of some text, possibly with some concepts
-    annotated as justifications for why particular pieces of the
-    summary were kept.
-
-    Attributes:
-     - summaryCommunication: Contains the text of the generated summary.
-     - concepts: Concepts mentioned in the summary which are believed to be
-    interesting and/or worth highlighting.
-    """
-
-    thrift_spec = (
-        None,  # 0
-        (1, TType.STRUCT, 'summaryCommunication', (concrete.communication.ttypes.Communication, concrete.communication.ttypes.Communication.thrift_spec), None, ),  # 1
-        (2, TType.LIST, 'concepts', (TType.STRUCT, (SummaryConcept, SummaryConcept.thrift_spec), False), None, ),  # 2
-    )
-
-    def __init__(self, summaryCommunication=None, concepts=None,):
-        self.summaryCommunication = summaryCommunication
-        self.concepts = concepts
-
-    def read(self, iprot):
-        if iprot._fast_decode is not None and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None:
-            iprot._fast_decode(self, iprot, (self.__class__, self.thrift_spec))
-            return
-        iprot.readStructBegin()
-        while True:
-            (fname, ftype, fid) = iprot.readFieldBegin()
-            if ftype == TType.STOP:
-                break
-            if fid == 1:
-                if ftype == TType.STRUCT:
-                    self.summaryCommunication = concrete.communication.ttypes.Communication()
-                    self.summaryCommunication.read(iprot)
-                else:
-                    iprot.skip(ftype)
-            elif fid == 2:
-                if ftype == TType.LIST:
-                    self.concepts = []
-                    (_etype17, _size14) = iprot.readListBegin()
-                    for _i18 in range(_size14):
-                        _elem19 = SummaryConcept()
-                        _elem19.read(iprot)
-                        self.concepts.append(_elem19)
-                    iprot.readListEnd()
-                else:
-                    iprot.skip(ftype)
-            else:
-                iprot.skip(ftype)
-            iprot.readFieldEnd()
-        iprot.readStructEnd()
-
-    def write(self, oprot):
-        if oprot._fast_encode is not None and self.thrift_spec is not None:
-            oprot.trans.write(oprot._fast_encode(self, (self.__class__, self.thrift_spec)))
-            return
-        oprot.writeStructBegin('Summary')
-        if self.summaryCommunication is not None:
-            oprot.writeFieldBegin('summaryCommunication', TType.STRUCT, 1)
-            self.summaryCommunication.write(oprot)
-            oprot.writeFieldEnd()
-        if self.concepts is not None:
-            oprot.writeFieldBegin('concepts', TType.LIST, 2)
-            oprot.writeListBegin(TType.STRUCT, len(self.concepts))
-            for iter20 in self.concepts:
-                iter20.write(oprot)
-            oprot.writeListEnd()
-            oprot.writeFieldEnd()
-        oprot.writeFieldStop()
-        oprot.writeStructEnd()
-
-    def validate(self):
-        return
-
-    def __repr__(self):
-        L = ['%s=%r' % (key, value)
-             for key, value in self.__dict__.items()]
-        return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
-
-    def __eq__(self, other):
-        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
-
-    def __ne__(self, other):
-        return not (self == other)
-
-
 class SummaryConcept(object):
     """
     A mention of a concept described in a summary which is thought
@@ -353,6 +266,93 @@ class SummaryConcept(object):
         if self.utility is not None:
             oprot.writeFieldBegin('utility', TType.DOUBLE, 4)
             oprot.writeDouble(self.utility)
+            oprot.writeFieldEnd()
+        oprot.writeFieldStop()
+        oprot.writeStructEnd()
+
+    def validate(self):
+        return
+
+    def __repr__(self):
+        L = ['%s=%r' % (key, value)
+             for key, value in self.__dict__.items()]
+        return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
+
+    def __ne__(self, other):
+        return not (self == other)
+
+
+class Summary(object):
+    """
+    A shortened version of some text, possibly with some concepts
+    annotated as justifications for why particular pieces of the
+    summary were kept.
+
+    Attributes:
+     - summaryCommunication: Contains the text of the generated summary.
+     - concepts: Concepts mentioned in the summary which are believed to be
+    interesting and/or worth highlighting.
+    """
+
+    thrift_spec = (
+        None,  # 0
+        (1, TType.STRUCT, 'summaryCommunication', (concrete.communication.ttypes.Communication, concrete.communication.ttypes.Communication.thrift_spec), None, ),  # 1
+        (2, TType.LIST, 'concepts', (TType.STRUCT, (SummaryConcept, SummaryConcept.thrift_spec), False), None, ),  # 2
+    )
+
+    def __init__(self, summaryCommunication=None, concepts=None,):
+        self.summaryCommunication = summaryCommunication
+        self.concepts = concepts
+
+    def read(self, iprot):
+        if iprot._fast_decode is not None and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None:
+            iprot._fast_decode(self, iprot, (self.__class__, self.thrift_spec))
+            return
+        iprot.readStructBegin()
+        while True:
+            (fname, ftype, fid) = iprot.readFieldBegin()
+            if ftype == TType.STOP:
+                break
+            if fid == 1:
+                if ftype == TType.STRUCT:
+                    self.summaryCommunication = concrete.communication.ttypes.Communication()
+                    self.summaryCommunication.read(iprot)
+                else:
+                    iprot.skip(ftype)
+            elif fid == 2:
+                if ftype == TType.LIST:
+                    self.concepts = []
+                    (_etype17, _size14) = iprot.readListBegin()
+                    for _i18 in range(_size14):
+                        _elem19 = SummaryConcept()
+                        _elem19.read(iprot)
+                        self.concepts.append(_elem19)
+                    iprot.readListEnd()
+                else:
+                    iprot.skip(ftype)
+            else:
+                iprot.skip(ftype)
+            iprot.readFieldEnd()
+        iprot.readStructEnd()
+
+    def write(self, oprot):
+        if oprot._fast_encode is not None and self.thrift_spec is not None:
+            oprot.trans.write(oprot._fast_encode(self, (self.__class__, self.thrift_spec)))
+            return
+        oprot.writeStructBegin('Summary')
+        if self.summaryCommunication is not None:
+            oprot.writeFieldBegin('summaryCommunication', TType.STRUCT, 1)
+            self.summaryCommunication.write(oprot)
+            oprot.writeFieldEnd()
+        if self.concepts is not None:
+            oprot.writeFieldBegin('concepts', TType.LIST, 2)
+            oprot.writeListBegin(TType.STRUCT, len(self.concepts))
+            for iter20 in self.concepts:
+                iter20.write(oprot)
+            oprot.writeListEnd()
             oprot.writeFieldEnd()
         oprot.writeFieldStop()
         oprot.writeStructEnd()

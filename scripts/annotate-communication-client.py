a0e53d5813bc75eb40d107b204eade5c04e7c69e
Craig Harman <craig@craigharman.net>
2019-02-26 10:48:29 -0500

    Added THttp support to annotate-communication-client.py

--- scripts/annotate-communication-client.py
+++ scripts/annotate-communication-client.py
@@ -4,9 +4,15 @@ from __future__ import unicode_literals
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 import logging
 
+from thrift.protocol.TProtocol import TProtocolException
+from thrift.transport.TTransport import TTransportException
+
 import concrete.version
 from concrete.util import CommunicationReader, CommunicationWriter, FileType
-from concrete.util.annotate_wrapper import AnnotateCommunicationClientWrapper
+from concrete.util.annotate_wrapper import (
+    AnnotateCommunicationClientWrapper,
+    HTTPAnnotateCommunicationClientWrapper
+)
 from concrete.util import set_stdout_encoding
 
 
@@ -15,13 +21,17 @@ def main():
 
     parser = ArgumentParser(
         formatter_class=ArgumentDefaultsHelpFormatter,
-        description="Interface with a Concrete AnnotateCommunicationService"
+        description=
+        "Interface with a Concrete AnnotateCommunicationService server. "
+        "Supports either THttp/TJSONProtocol (using the '--uri' flag) "
+        "or TSocket/TCompactProtocol (using '--host'/'--port')"
     )
-    parser.add_argument('host',
-                        help="Hostname of annotate service to which to"
-                             " connect.")
-    parser.add_argument('port', type=int,
-                        help="Port of annotate service to which to connect.")
+    parser.add_argument('--host', default='localhost',
+                        help="Hostname of TSocket/TCompactProtocol AnnotateCommunicationService")
+    parser.add_argument('-p', '--port', type=int, default=9090,
+                        help="Port of TSocket/TCompactProtocol AnnotateCommunicationService")
+    parser.add_argument('--uri', '--url',
+                        help="URI of THttpServer/TJSONProtocol AnnotateCommunicationService")
     parser.add_argument('-l', '--loglevel', '--log-level',
                         help='Logging verbosity level threshold (to stderr)',
                         default='info')
@@ -47,10 +57,25 @@ def main():
     output_path = '/dev/fd/1' if args.output == '-' else args.output
 
     reader = CommunicationReader(input_path, **reader_kwargs)
-    with AnnotateCommunicationClientWrapper(args.host, args.port) as client:
-        with CommunicationWriter(output_path) as writer:
-            for (comm, _) in reader:
-                writer.write(client.annotate(comm))
+    if args.uri:
+        try:
+            with HTTPAnnotateCommunicationClientWrapper(args.uri) as client:
+                with CommunicationWriter(output_path) as writer:
+                    for (comm, _) in reader:
+                        writer.write(client.annotate(comm))
+        except TProtocolException as ex:
+            logging.error(ex)
+            logging.error(
+                "Please check if the URI '{}' points to an AnnotateCommunicationService "
+                "using the Thrift THttp transport and TJSONProtocol encoding".format(args.uri))
+    else:
+        try:
+            with AnnotateCommunicationClientWrapper(args.host, args.port) as client:
+                with CommunicationWriter(output_path) as writer:
+                    for (comm, _) in reader:
+                        writer.write(client.annotate(comm))
+        except (ConnectionRefusedError, TTransportException) as ex:
+            pass
 
 
 if __name__ == "__main__":
